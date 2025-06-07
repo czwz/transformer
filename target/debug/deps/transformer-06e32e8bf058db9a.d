9a1967a08873bcdd948978a275dece358c8f3500
czwz <tim010007@gmail.com>
2025-06-07 21:42:41 +0100

    [feature] first commit

---
+++ target/debug/deps/transformer-06e32e8bf058db9a.d
@@ -0,0 +1,5 @@
+/Users/raven/myProjects/transformer/target/debug/deps/libtransformer-06e32e8bf058db9a.rmeta: src/main.rs
+
+/Users/raven/myProjects/transformer/target/debug/deps/transformer-06e32e8bf058db9a.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/transformer-12c01eb4451f61db.d
@@ -0,0 +1,5 @@
+/Users/raven/myProjects/transformer/target/debug/deps/libtransformer-12c01eb4451f61db.rmeta: src/main.rs
+
+/Users/raven/myProjects/transformer/target/debug/deps/transformer-12c01eb4451f61db.d: src/main.rs
+
+src/main.rs:

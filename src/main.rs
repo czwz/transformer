923f65973ee3b93b7e5929b8bd93f5299fcbacd4
czwz <tim010007@gmail.com>
2025-06-14 16:56:20 +0100

    [feature] test on main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
+use candle_core::Device;
+use tokenizers::tokenizer::Tokenizer;
+
 mod embeddings;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let tokenizer = Tokenizer::from_file("./wordlevel-wiki.json")?;
+    let encoding = tokenizer.encode(
+        ("Hello, world", "I am raven"), 
+        true
+    )?;
+
+    println!("tok: {:?}", encoding.get_tokens());
+    println!("ids: {:?}", encoding.get_ids());
+
+    let e = embeddings::postional_embeddings::PositionalEmbeddings::new(
+        512,
+        768,
+        candle_nn::Dropout::new(0.1),
+        &Device::Cpu,
+    ).unwrap();
+
+    println!("Positional Embeddings: {:?}", e.positional_embeddings);
+
+    Ok(())
 }

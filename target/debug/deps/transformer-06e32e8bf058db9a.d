dc7123f3d845e0a6ea0b5d80cab0299da2335403
czwz <tim010007@gmail.com>
2025-06-14 15:42:31 +0100

    [feature] clean up

--- target/debug/deps/transformer-06e32e8bf058db9a.d
+++
@@ -1,5 +0,0 @@
-/Users/raven/myProjects/transformer/target/debug/deps/libtransformer-06e32e8bf058db9a.rmeta: src/main.rs
-
-/Users/raven/myProjects/transformer/target/debug/deps/transformer-06e32e8bf058db9a.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/transformer-12c01eb4451f61db.d
+++
@@ -1,5 +0,0 @@
-/Users/raven/myProjects/transformer/target/debug/deps/libtransformer-12c01eb4451f61db.rmeta: src/main.rs
-
-/Users/raven/myProjects/transformer/target/debug/deps/transformer-12c01eb4451f61db.d: src/main.rs
-
-src/main.rs:
